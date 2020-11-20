e2cc1ef5e6140aedabd771b5464c10f7435ef6d6
Shiree Hughes <shireeh@g.clemson.edu>
2020-11-19 19:37:43 -0500

    Start LinkedList Practice
    Start with SinglyLinkedLists

---
+++ C++/LinkedLists/LinkedLists/SinglyLinkedList.cpp
@@ -0,0 +1,112 @@
+//
+//  SinglyLinkedList.cpp
+//  LinkedLists
+//
+//  Created by Shiree Hughes on 11/19/20.
+//
+
+#include "SinglyLinkedList.hpp"
+#include <iostream>
+
+SinglyLinkedList::~SinglyLinkedList() {
+    delete root;
+}
+SinglyLinkedList::SinglyLinkedList() {
+    root = NULL;
+}
+
+void SinglyLinkedList::addNode(Node * n) {
+    if(root == NULL) {
+        root = n;
+        return;
+    }
+    
+    Node * current = root;
+    while(current->next != NULL) {
+        current = current->next;
+    }
+    current->next = n;
+}
+
+void SinglyLinkedList::addVal(int v) {
+    Node * newNode = new Node();
+    newNode->value = v;
+    newNode->next = NULL;
+    
+    addNode(newNode);
+}
+
+bool SinglyLinkedList::isInList(int v) {
+    Node * current = root;
+    while(current != NULL) {
+        if(current->value == v){
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+bool SinglyLinkedList::remove(int n) {
+    if(root->value == n) {
+        root = root->next;
+        return true;
+    }
+
+    Node * current = root;
+    while(current->next != NULL) {
+        if(current->next->value == n){
+            current->next = current->next->next;
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+bool SinglyLinkedList::removeNode(Node * n) {
+    if(root == n) {
+        root = n->next;
+        return true;
+    }
+
+    Node * current = root;
+    while(current->next != NULL) {
+        if(current->next == n){
+            current->next = current->next->next;
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+void SinglyLinkedList::reverse() {
+    Node * prev = root;
+    Node * current = root->next;
+
+    while(current->next != NULL) {
+        Node * temp = current->next;
+        current->next = prev;
+        prev = current;
+        current = temp;
+    }
+    
+    current->next = prev;
+    root->next = NULL;
+    root = current;
+}
+
+void SinglyLinkedList::clear() {
+    root = NULL;
+}
+
+void SinglyLinkedList::printList() {
+    Node * current = root;
+    while(current != NULL) {
+        std::cout << current->value << " ";
+        current = current->next;
+    }
+    std::cout << "\n";
+}
+
